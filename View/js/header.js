faa2d49c6e61741ed1212b0b4139f53e8e40101a
CorineTang <tang.corine@gmail.com>
2015-06-25 09:29:31 +0200

    Solution temporaire gestion role

--- View/js/header.js
+++ View/js/header.js
@@ -12,33 +12,30 @@ function initializeHeader(idRole) {
 	accueil-option
 	profil-option
 	*/
-	if (idRole === 3) { // Admin
-		$('ul.utilisateur-option').removeClass('hide-option');
-		$('li.accueil-option').removeClass('hide-option');
-		$('li.admin-option').removeClass('hide-option');
-		$('li.profil-option').removeClass('hide-option');
-	}
-	if (idRole === 2) { // Adhérent
-		$('ul.utilisateur-option').removeClass('hide-option');
-		$('li.profil-option').removeClass('hide-option');
-		$('li.stage-option').removeClass('hide-option');
-		$('li.stage-voeux-option').removeClass('hide-option');
-		$('li.stage-consultation-option').removeClass('hide-option');
-		$('li.stage-evaluation-option').removeClass('hide-option');	
-	}
-	if (idRole === 1) { // Interne
-		$('ul.utilisateur-option').removeClass('hide-option');
-		$('li.profil-option').removeClass('hide-option');
-		$('li.stage-option').removeClass('hide-option');
-		$('li.stage-voeux-option').removeClass('hide-option');
-		$('li.stage-consultation-option').removeClass('hide-option');		
-	}
-    else{ // Visiteur
-        $('ul.utilisateur-option').removeClass('hide-option');
-		$('li.profil-option').removeClass('hide-option');
-		$('li.stage-option').removeClass('hide-option');
-		$('li.stage-voeux-option').removeClass('hide-option');
-    }
+	// if (idRole === 3) { // Admin
+	// 	$('ul.utilisateur-option').removeClass('hide-option');
+		
+	// 	$('li.admin-option').removeClass('hide-option');
+	// 	$('li.profil-option').removeClass('hide-option');
+	// }
+	// if (idRole === 2) { // Adhérent
+	// 	$('ul.utilisateur-option').removeClass('hide-option');
+	// 	$('li.profil-option').removeClass('hide-option');
+	// 	$('li.stage-option').removeClass('hide-option');
+	// 	$('li.stage-voeux-option').removeClass('hide-option');
+	// 	$('li.stage-consultation-option').removeClass('hide-option');
+	// 	$('li.stage-evaluation-option').removeClass('hide-option');	
+	// }
+	// if (idRole === 1) { // Interne
+	// 	$('ul.utilisateur-option').removeClass('hide-option');
+	// 	$('li.profil-option').removeClass('hide-option');
+	// 	$('li.stage-option').removeClass('hide-option');
+	// 	$('li.stage-voeux-option').removeClass('hide-option');
+	// 	$('li.stage-consultation-option').removeClass('hide-option');		
+	// }
+ //    else{ // Visiteur
+ //    	$('li.accueil-option').removeClass('hide-option');
+ //    }
 }
 
 jQuery(function(){
