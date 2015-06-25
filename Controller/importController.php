da6babff19e441544118cd4be0b29018a30231dd
asaudemont1 <saudemont.aurelien@gmail.com>
2015-06-25 10:12:32 +0200

    MAJ importModel

--- Controller/importController.php
+++ Controller/importController.php
@@ -22,21 +22,33 @@ function importerClassement() {
 	// UPLOAD DU FICHIER CSV, vérification et insertion en BASE
 		if($_FILES["file"]["type"] != "application/vnd.ms-excel"){
 			die("Ce n'est pas un fichier de type .csv");
-		}
+		}#End if
+
 		elseif(is_uploaded_file($_FILES['file']['tmp_name'])){
 			require('Model/importModel.php');
 			importClassement();
 		}
 		else{
 			die("Vous ne devriez pas être là");
-		}
+		}#End elseif
 }
 
 /*
  * Import du fichier stage côté serveur
  */
 function importerStage() {
-		
+		// UPLOAD DU FICHIER CSV, vérification et insertion en BASE
+		if($_FILES["file"]["type"] != "application/vnd.ms-excel"){
+			die("Ce n'est pas un fichier de type .csv");
+		}#End if
+
+		elseif(is_uploaded_file($_FILES['file']['tmp_name'])){
+			require('Model/importModel.php');
+			importStages();
+		}
+		else{
+			die("Vous ne devriez pas être là");
+		}#End elseif
 }
 
 ?>
