5a8dcb137147948f3f24cd3449a4594d9e0177f4
amandlabeautin <amandlabeautin@gmail.com>
2015-06-26 14:07:29 +0200

    Sortable JQuery

--- View/js/pageAccueilConnect.js
+++ View/js/pageAccueilConnect.js
@@ -6,3 +6,10 @@ jQuery(function(){
     $("#AffichageStage2").onclick(function(){
         document.getElementById("informationStage2").style.visibility = "visible";
     });
+    $( ".portlet" ).onload(function(){
+      .addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
+      .find( ".portlet-header" )
+        .addClass( "ui-widget-header ui-corner-all" )
+        .prepend( "<span class='ui-icon ui-icon-minusthick portlet-toggle'></span>");
+    });
+}
