74617bdc4e91d7b19b1804e63e1094bfee9048d6
Corine Tang <ctang@advanced-schema.com>
2015-06-27 15:46:33 +0200

    Fixed syntax error in pageAccueilConnect.js

--- View/js/pageAccueilConnect.js
+++ View/js/pageAccueilConnect.js
@@ -1,15 +1,15 @@
 jQuery(function(){
-    $("#AffichageStage2").onload(function(){
+    $("#AffichageStage2").ready(function(){
         document.getElementById("informationStage2").style.visibility = "hidden";
     });
-    
-    $("#AffichageStage2").onclick(function(){
+
+    $("#AffichageStage2").click(function(){
         document.getElementById("informationStage2").style.visibility = "visible";
     });
-    $( ".portlet" ).onload(function(){
-      .addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
+    $( ".portlet" ).ready(function(){
+      $(this).addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
       .find( ".portlet-header" )
         .addClass( "ui-widget-header ui-corner-all" )
         .prepend( "<span class='ui-icon ui-icon-minusthick portlet-toggle'></span>");
     });
-}
+});
