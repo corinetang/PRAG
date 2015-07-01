094638e28a131ddae4605f0f1efc67476973ea4a
amandlabeautin <amandlabeautin@gmail.com>
2015-07-01 14:00:23 +0200

    Message après import

--- Controller/importController.php
+++ Controller/importController.php
@@ -32,7 +32,18 @@ function importerClassement() {
 
 		elseif(is_uploaded_file($_FILES['file']['tmp_name'])){
 			require('Model/importModel.php');
-			importClassement();
+			if(importClassement()){
+				echo ('<div class="alert alert-dismissible alert-success">
+  <button type="button" class="close" data-dismiss="alert">×</button>
+  <strong>Bravo!</strong>L\'import s\'est passé avec success. <a href="index.php" class="alert-link"><br>Retour à la page d\'accueil</a>.
+</div>')
+			}
+			else{
+				echo ('<div class="alert alert-dismissible alert-danger">
+  <button type="button" class="close" data-dismiss="alert">×</button>
+  <strong>Mince!</strong> <a href="index.php?control=import&action=showImportClassement" class="alert-link">Un problème est survenu lors de l\'import du fichier. </a> Réessayez ! .
+</div>')
+			}
 		}
 		else{
 			die("Vous ne devriez pas être là");
@@ -52,8 +63,17 @@ function importerStage() {
 
 		elseif(is_uploaded_file($_FILES['file']['tmp_name'])){
 			require('Model/importModel.php');
-			importStages($Filiere);
-		}
+			if(importStages($Filiere)) {
+				echo ('<div class="alert alert-dismissible alert-success">
+			  <button type="button" class="close" data-dismiss="alert">×</button>
+			  <strong>Bravo!</strong>L\'import s\'est passé avec success. <a href="index.php" class="alert-link"><br>Retour à la page d\'accueil</a>.
+			</div>')
+			}else {
+			echo ('<div class="alert alert-dismissible alert-danger">
+			  <button type="button" class="close" data-dismiss="alert">×</button>
+			  <strong>Mince!</strong> <a href="index.php?control=import&action=showImportStage" class="alert-link">Un problème est survenu lors de l\'import du fichier. </a> Réessayez ! .
+			</div>')
+			}#End if
 		else{
 			die("Vous ne devriez pas être là");
 		}#End elseif
